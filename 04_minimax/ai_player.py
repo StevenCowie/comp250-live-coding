1ddcb130ab82cc0bb0f0eb1910fafcc411a20b2f
Ed Powley <edpowley@gmail.com>
2018-02-18 15:39:50 +0000

    Removed minimax implementation

--- 04_minimax/ai_player.py
+++ 04_minimax/ai_player.py
@@ -1,47 +1,8 @@
 import random
 
 
-def minimax(board):
-    result = board.get_result(2)
-    if result is not None:
-        return result
-    else:
-        if board.current_player == 1:
-            best_value = 1000
-        else:
-            best_value = -1000
-
-        for move in board.get_moves():
-            board.do_move(move)
-            value = minimax(board)
-            board.undo_move(move)
-            if board.current_player == 1:
-                best_value = min(value, best_value)
-                if best_value == -1:
-                    return best_value
-            else:
-                best_value = max(value, best_value)
-                if best_value == 1:
-                    return best_value
-
-        return best_value
-
-
 def choose_move(board):
-    """ Takes a game board, and returns a move to play as a tuple (x,y)
+    """ Takes a game board, and returns a move to play
     """
 
-    best_move = None
-    best_value = -1000
-
-    for move in board.get_moves():
-        board.do_move(move)
-        value = minimax(board)
-        board.undo_move(move)
-        print move, value
-        if value > best_value:
-            best_move = move
-            best_value = value
-
-    print
-    return best_move
+    return random.choice(board.get_moves())
